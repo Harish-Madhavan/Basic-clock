3f9eacc3e78a856b03a7955ce251bcda2f93b832
Bionic-Squash <111076574+Bionic-Squash@users.noreply.github.com>
2023-06-26 21:11:56 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,16 +1,20 @@
 from tkinter import *
-from tkinter.ttk import *
 from time import strftime
+import customtkinter
 
-root = Tk()
+#customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+#customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+root = customtkinter.CTk()
 root.title("clock")
 
 def TIME():
     STR11= strftime('%H:%M:%S:%p')
-    label.config(text=STR11)
+    label.configure(text=STR11)
     label.after(1000,TIME)
 
-label = Label(root,font=("Helvetica",80),background="black",foreground="blue")
+#label = Label(root,font=("Helvetica",80),background="black",foreground="blue")
+label = customtkinter.CTkLabel(root,font=("Helvetica",80), fg_color="blue")
 label.pack(anchor='center')
 TIME()
-mainloop()
+root.mainloop()
