8c6e446a491fce0f7b58036bdd93e316c19d183b
Bionic-Squash <111076574+Bionic-Squash@users.noreply.github.com>
2023-07-01 20:03:48 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -2,9 +2,6 @@ from tkinter import *
 from time import strftime
 import customtkinter
 
-#customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
-#customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
-
 root = customtkinter.CTk()
 root.title("clock")
 
@@ -13,7 +10,6 @@ def TIME():
     label.configure(text=STR11)
     label.after(1000,TIME)
 
-#label = Label(root,font=("Helvetica",80),background="black",foreground="blue")
 label = customtkinter.CTkLabel(root,font=("Helvetica",80), fg_color="blue")
 label.pack(anchor='center')
 TIME()
